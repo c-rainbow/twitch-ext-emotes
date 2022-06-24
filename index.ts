b2fb4ef5a419e986e75a25428e026c6c345f2e64
Rainbow <46095062+c-rainbow@users.noreply.github.com>
2022-06-23 19:56:51 -0700

    Exported all types and classes in index.ts

---
+++ index.ts
@@ -0,0 +1,5 @@
+import { Emote, BttvEmote, FfzEmote, SevenTvEmote } from "./types";
+import { EmoteManager } from "./manager";
+
+
+export { Emote, BttvEmote, FfzEmote, SevenTvEmote, EmoteManager };
