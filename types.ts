7b5738ebe7105bb78637ef494bec53df1c46ba4c
Rainbow <46095062+c-rainbow@users.noreply.github.com>
2022-06-23 17:54:45 -0700

    Create fetcher from BTTV, FFZ, and 7TV

---
+++ types.ts
@@ -0,0 +1,33 @@
+
+export type EmoteSource = 'twitch' | 'bttv' | 'ffz' | '7tv';
+
+export interface Emote {
+  source: EmoteSource;  // From which emote provider?
+  id: string;  // provider-specific unique ID 
+  text: string;  //  text of the emote (ex: "BibleThump")
+  url: string;  // Default URL of the emote pic
+}
+
+export interface TwitchEmote extends Emote {
+  source: 'twitch';
+}
+
+export interface BttvEmote extends Emote {
+  source: 'bttv';
+}
+
+
+export interface FfzEmote extends Emote {
+  source: 'ffz';
+}
+
+
+export interface SevenTvEmote extends Emote {
+  source: '7tv';
+}
+
+
+export interface ChatFragment {
+  text: string;
+  emote: Emote;
+}
